06a908f83d099f09d1e47b6a54f78e2ca98051d9
Dominic Letz <dominic.letz@berlin.de>
2024-10-21 23:16:03 +0200

    v1.0.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Timeseriesdb.MixProject do
   def project do
     [
       app: :timeseriesdb,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
